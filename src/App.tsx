a08ff4c1167e6c3b0ed382269281f8e3f6ff5fc9
Ego Maragustaf <egomaragustaf@gmail.com>
2023-11-15 14:54:52 +0700

    feat(example): implement button shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "~/components/ui/button";
+
 export default function App() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="flex flex-col justify-center items-center gap-2 min-h-screen">
       <h1 className="text-indigo-700 text-4xl font-semibold">Web Sekolah</h1>
+      <Button className="bg-indigo-700 hover:bg-indigo-800 px-8">Login</Button>
     </div>
   );
 }
