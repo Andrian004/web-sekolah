741aa9fa1b06d664777cb6fbbf7af6f7cc6a7d5c
Andrian004 <128672158+Andrian004@users.noreply.github.com>
2023-11-18 23:32:37 +0700

    chore: add background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,6 +72,9 @@ export default {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      backgroundImage: {
+        "hero-pattern": "url('~/assets/school-building.jpg')",
+      },
     },
   },
   plugins: [tailwindAnimate],
