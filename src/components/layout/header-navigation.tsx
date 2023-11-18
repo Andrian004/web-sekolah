197a07bc2cdd406db41986842511fbf701cc6f81
Andrian004 <128672158+Andrian004@users.noreply.github.com>
2023-11-18 22:30:05 +0700

    feat: update header-navigation

--- src/components/layout/header-navigation.tsx
+++ src/components/layout/header-navigation.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import {
   HoverCard,
@@ -19,21 +20,22 @@ type NavItem = {
 
 const navPublicItems: NavItem[] = [
   { text: "Beranda", path: "/", dropdown: false },
-  { text: "Artikel", path: "/news", dropdown: false },
+  { text: "Berita", path: "/news", dropdown: false },
+  { text: "Agenda", path: "/plan", dropdown: false },
   {
     text: "Staff",
     dropdown: true,
     dropdownItems: [
-      { text: "Pendidik", path: "/news" },
-      { text: "Non Pendidik", path: "/news" },
+      { text: "Pendidik", path: "/staff" },
+      { text: "Non Pendidik", path: "/staff" },
     ],
   },
   {
     text: "Tentang",
     dropdown: true,
     dropdownItems: [
-      { text: "Sejarah", path: "/news" },
-      { text: "Contact", path: "/news" },
+      { text: "Sejarah", path: "/about" },
+      { text: "Contact", path: "/about" },
     ],
   },
 ];
@@ -52,33 +54,64 @@ export function HeaderNavigation() {
 }
 
 export function NavigationList({ navItems }: { navItems: NavItem[] }) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [listIndex, setListIndex] = useState<number>(0);
+
   return (
     <ul className="flex gap-8">
-      {navItems.map((navItem) => {
+      {navItems.map((navItem, index) => {
         return (
-          <li key={navItem.text} className="px-4 py-2 rounded-md">
+          <li key={index} className="px-4 py-2 rounded-md">
             {!navItem.dropdown ? (
-              <NavLink to={navItem.path ? navItem.path : ""}>
+              <NavLink
+                to={navItem.path ? navItem.path : ""}
+                className={({ isActive }) =>
+                  isActive ? "text-black" : "text-white"
+                }
+                end
+              >
                 {navItem.text}
               </NavLink>
             ) : (
-              <HoverCard openDelay={300} closeDelay={150}>
-                <HoverCardTrigger className="hover:cursor-pointer">
+              <HoverCard
+                openDelay={200}
+                closeDelay={100}
+                onOpenChange={(e) => {
+                  setIsOpen(e);
+                  setListIndex(index);
+                }}
+              >
+                <HoverCardTrigger className="hover:cursor-pointer flex gap-1">
                   {navItem.text}
+                  <div
+                    className={
+                      isOpen && index === listIndex
+                        ? "rotate-180 ease-linear duration-300"
+                        : "ease-linear duration-300"
+                    }
+                  >
+                    <i className={`bi bi-caret-down-fill`}></i>
+                  </div>
                 </HoverCardTrigger>
                 <HoverCardContent className="mt-6">
-                  <ul>
+                  <div className="flex flex-col">
                     {navItem.dropdownItems?.map((item, index) => {
                       return (
-                        <li
+                        <NavLink
+                          to={item.path}
                           key={index}
-                          className="mb-2 p-1 rounded hover:bg-primary hover:text-white"
+                          className={({ isActive }) =>
+                            isActive
+                              ? "mb-2 p-1 rounded hover:bg-primary hover:text-white bg-primary"
+                              : "mb-2 p-1 rounded hover:bg-primary hover:text-white"
+                          }
+                          end
                         >
-                          <NavLink to={item.path}>{item.text}</NavLink>
-                        </li>
+                          {item.text}
+                        </NavLink>
                       );
                     })}
-                  </ul>
+                  </div>
                 </HoverCardContent>
               </HoverCard>
             )}
