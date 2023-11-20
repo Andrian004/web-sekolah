35c4c641b53c42f8449c51217a3fa1151409bbb8
Ego Maragustaf <egomaragustaf@gmail.com>
2023-11-20 22:00:55 +0700

    style: remove font weight

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
   const year = date.getFullYear();
 
   return (
-    <footer className="w-full bottom-0 flex items-center justify-center bg-primary text-white py-4 text-base font-medium h-20">
+    <footer className="w-full bottom-0 flex items-center justify-center bg-primary text-white py-4 text-base h-20">
       <section className="w-full max-w-6xl">
         <span>Web Sekolah. All rights reserved &copy; {year}</span>
       </section>
