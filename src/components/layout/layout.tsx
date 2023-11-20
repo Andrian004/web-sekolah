d5cc6cc7d68d93736db8991c74a9639bf3a56daf
Ego Maragustaf <egomaragustaf@gmail.com>
2023-11-20 22:57:01 +0700

    feat: add Open Sans font as brand font

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export function Layout({ children }: Props) {
   return (
-    <div className="w-full">
+    <div className="w-full font-brand">
       <HeaderNavigation />
       <main className="flex flex-col justify-center items-center gap-2 min-h-screen mb-96">
         {children}
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,9 @@ export default {
       },
     },
     extend: {
+      fontFamily: {
+        brand: ["Open Sans", "sans-serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
