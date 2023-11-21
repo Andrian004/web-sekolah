a704d950767bc74e3a678a4e5fdc899e05d2f028
Andrian004 <128672158+Andrian004@users.noreply.github.com>
2023-11-21 16:37:14 +0700

    feat: improve responsiveness

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./styles/global.css";
-import NewsPage from "~/routes/user/news";
-import HomePage from "~/routes/user/home";
-import Dashboard from "~/routes/admin/dashboard";
-import PlanPage from "~/routes/user/plan";
-import StaffPage from "~/routes/user/staff";
-import AboutPage from "~/routes/user/about";
+import { Dashboard } from "./routes/admin";
+import {
+  NewsPage,
+  HomePage,
+  PlanPage,
+  StaffPage,
+  AboutPage,
+} from "~/routes/user";
 
 const router = createBrowserRouter([
   {
