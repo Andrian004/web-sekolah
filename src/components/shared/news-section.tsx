100da32c6fb927516f7c5c182f3ba8ba8bca1b51
Ego Maragustaf <egomaragustaf@gmail.com>
2023-11-29 15:59:25 +0700

    style: text color title news section
    fix: change auth api with get parameter

--- src/components/shared/news-section.tsx
+++ src/components/shared/news-section.tsx
@@ -6,7 +6,7 @@ export function NewsSection() {
 
   return (
     <>
-      <h2 className="my-10 font-bold">Berita Terbaru</h2>
+      <h2 className="my-10 font-bold text-emerald-900">Berita Terbaru</h2>
       <div className="flex justify-between items-center flex-wrap">
         {news.isLoading && <div className="py-2 font-bold">Loading...</div>}
         {news.error instanceof Error && <div>{news.error.message}</div>}
