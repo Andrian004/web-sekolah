64330de514d3e763f6c9bb76c9a27945f2169859
Ego Maragustaf <egomaragustaf@gmail.com>
2024-01-02 23:58:08 +0700

    feat: add profile site footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,4 +1,53 @@
+import {
+  TbBrandFacebookFilled,
+  TbBrandInstagram,
+  TbBrandX,
+  TbBrandYoutubeFilled,
+} from "react-icons/tb";
+
 import { getCurrentYear } from "~/utils/datetime";
+import { Separator } from "~/components/ui/separator";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "~/components/ui/tooltip";
+import { cn } from "~/utils/cn";
+
+type Socmed = {
+  icon: React.ReactNode;
+  onHover: string;
+  link: string;
+  tooltip: string;
+};
+
+const SocmedList: Socmed[] = [
+  {
+    icon: <TbBrandFacebookFilled />,
+    onHover: "hover:text-blue-600",
+    link: "https://github.com/Andrian004",
+    tooltip: "Facebook",
+  },
+  {
+    icon: <TbBrandYoutubeFilled />,
+    onHover: "hover:text-red-600 ",
+    link: "https://github.com/Andrian004",
+    tooltip: "Youtube",
+  },
+  {
+    icon: <TbBrandInstagram />,
+    onHover: "hover:text-pink-700 ",
+    link: "https://github.com/Andrian004",
+    tooltip: "Instagram",
+  },
+  {
+    icon: <TbBrandX />,
+    onHover: "hover:text-black",
+    link: "https://github.com/Andrian004",
+    tooltip: "X (aja)",
+  },
+];
 
 export function Footer() {
   const year = getCurrentYear();
@@ -20,12 +69,37 @@ export function Footer() {
 function ProfileSiteFooter() {
   return (
     <section className="w-80 flex flex-col items-center justify-center gap-4">
-      <h2>SMA Negeri 1 Surakarta</h2>
+      <h2 className="w-full font-semibold">SMA Negeri 1 Surakarta</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae vitae
         voluptate commodi eos maxime doloremque dolore reiciendis laudantium
         accusamus architecto.
       </p>
+      <Separator />
+      <div className="w-full flex">
+        <ul className="flex gap-2">
+          {SocmedList.map((item, index) => (
+            <TooltipProvider key={index} delayDuration={300}>
+              <Tooltip>
+                <TooltipTrigger>
+                  <li
+                    className={cn(
+                      `border border-white text-white p-2 rounded-full transition duration-300`,
+                      item.onHover
+                    )}>
+                    <a href={item.link} className="text-2xl">
+                      {item.icon}
+                    </a>
+                  </li>
+                </TooltipTrigger>
+                <TooltipContent side="bottom">
+                  <p>{item.tooltip}</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
