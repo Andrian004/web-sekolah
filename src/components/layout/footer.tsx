bbdaaf71887564e245328f787d16cdd148c9ebf0
Ego Maragustaf <egomaragustaf@gmail.com>
2024-01-02 22:22:39 +0700

    style: add padding right and left footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   const year = getCurrentYear();
 
   return (
-    <footer className="w-full bottom-0 flex items-center justify-center bg-primary text-white py-4 text-base h-20">
+    <footer className="w-full bottom-0 flex items-center justify-center bg-primary text-white py-4 text-base h-20 px-2">
       <section className="w-full max-w-6xl">
         <span>Web Sekolah. All rights reserved &copy; {year}</span>
       </section>
